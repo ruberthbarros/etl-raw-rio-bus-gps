fe1367adc4e9d899c3b587c2aec3da154954d11c
Ruberth <ruberth.barros@gazeus.com>
2020-04-12 21:41:55 -0300

    Adding request service and domain errors.

---
+++ collect_rio_gps_data/service/request_service.py
@@ -0,0 +1,36 @@
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.exceptions import Timeout
+from collect_rio_gps_data.domain import errors
+
+
+def get_gps_points(endpoint, max_retries=5, timeout=3):
+    """Gets data from data.rio gps API.
+
+    Args:
+        endpoint (str): the API endpoint.
+        max_retries (int): max number of retries in case of failure.
+        timeout (float): max time(in seconds) to wait for a response from API.
+
+    Returns:
+        dict: dictionary as below,
+        {
+            "columns": list of column names,
+            "rows": list of rows
+        }
+    """
+    with Session() as session_:
+        session_.mount(endpoint, HTTPAdapter(max_retries=max_retries))
+        response = session_.get(endpoint, timeout=timeout)
+
+        response.raise_for_status()
+
+    json_data = response.json()
+
+    if not json_data:
+        raise errors.EmptyResponseError()
+
+    return {
+        "columns": list(map(str.lower, json_data["COLUMNS"])),
+        "rows": json_data["DATA"]
+    }
